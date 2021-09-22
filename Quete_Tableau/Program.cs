c6d3cab7ba771d699ff89284e8f9936b09da9b83
JGouyau <Gouyau.jimmy@gmail.com>
2021-09-22 10:24:09 +0200

    add tableau fonction for quest

---
+++ Quete_Tableau/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quete_Tableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tailleDuTableau = 3;
+            int[] tableauUn = new int[tailleDuTableau];
+            int[] tableauDeux = new int[tailleDuTableau];
+            int[] grosTableau = new int[tailleDuTableau * 2];
+
+            var random = new Random();
+            for (int i = 0; i < tailleDuTableau; i++)
+            {
+                tableauUn[i] = random.Next(100);
+                Console.Write(tableauUn[i] + " : ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < tailleDuTableau; i++)
+            {
+                tableauDeux[i] = random.Next(100);
+                Console.Write(tableauDeux[i] + " : ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < tailleDuTableau; i++)
+            {
+                grosTableau[i] = tableauUn[i];
+                grosTableau[i + tailleDuTableau] = tableauDeux[i];
+            }
+            foreach (int element in grosTableau)
+            {
+                Console.Write(element + " : ");
+            }
+            Console.ReadKey();
+        } 
+    }
+}
